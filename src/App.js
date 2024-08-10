b71810db5178d96f18423e160ca313142f367d70
Tianchen zhang <jasontc196@gmail.com>
2024-08-10 16:10:08 -0400

    feat: Add login and registration routes
    This commit adds the login and registration routes to the application. It imports the necessary components for the login and registration pages and sets up the routing using React Router. This allows users to navigate to the login and registration pages and interact with the corresponding forms.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import LoginPage from './Webpage/Login_page';
+import RegisterPage from './Webpage/Register_page';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage/>} />
+        <Route path='Register' element={<RegisterPage/>}/>
+
+      </Routes>
+    </Router>
   );
 }
+
 
 export default App;
