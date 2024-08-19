a3cec54e3d4154a2895f2f7c686742160a9e3015
Tianchen zhang <jasontc196@gmail.com>
2024-08-19 16:17:44 -0400

    feat: 仅用于测试的静态数据，由于没有目前没有后端，暂时所有用于测试的静态数据都可放进这个文件夹

---
+++ src/static_data_for_test/ScheduleData.js
@@ -0,0 +1,29 @@
+//测试用静态数据
+const scheduleServices = [
+    {
+      title: 'Service 1',
+      date: '2024-08-20',
+      time: '09:00-10:00',
+    },
+
+    {
+      title: 'Service 2',
+      date: '2024-08-29',
+      time: '10:00-11:00',
+
+    },
+
+    {
+      title: 'Service 3',
+      date: '2024-09-03',
+      time: '11:00-12:00',
+    },
+
+    {
+      title: 'Service 4',
+      date: '2024-09-04',
+      time: '12:00-13:00',
+    },
+  ];
+
+  export default scheduleServices;
